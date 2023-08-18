191d41c62ea0ae9d07cbd05c6eecaaf6271c01ef
Oğuzhan <aydinayoguzhan@gmail.com>
2023-08-18 18:07:51 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/Contexts/EfContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class EfContext : DbContext
+    {
+        public EfContext(DbContextOptions<EfContext> options) : base(options)
+        {
+        }
+    }
+}
