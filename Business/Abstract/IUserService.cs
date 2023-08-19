375215d07d4c9d7fce1d47ce479c432eb13af804
Oğuzhan <aydinayoguzhan@gmail.com>
2023-08-19 13:14:28 +0300

    User business katmanının result sistemine göre return yapması sağlandı ve messages eklendi

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities.Concrete;
+using Core.Utilities.Results.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,6 @@ namespace Business.Abstract
 {
     public interface IUserService
     {
-        User GetByMail(string email);
+        IDataResult<User> GetByMail(string email);
     }
 }
--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
+using Business.Constants;
 using Core.Entities.Concrete;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
 using System;
 using System.Collections.Generic;
@@ -11,16 +14,16 @@ namespace Business.Concrete
 {
     public class UserManager : IUserService
     {
-        IUserDal _userDal;
+        private IUserDal _userDal;
 
         public UserManager(IUserDal userDal)
         {
             _userDal = userDal;
         }
 
-        public User GetByMail(string email)
+        public IDataResult<User> GetByMail(string email)
         {
-            return _userDal.Get(x => x.Email == email);
+            return new SuccessDataResult<User>(_userDal.Get(x => x.Email == email), Messages.Successful);
         }
     }
 }
